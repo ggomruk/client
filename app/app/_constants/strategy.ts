2268bf36484576ba07c8493745b9fdd158947e8a
eqfwcev123 <dohyeonee95@hotmail.com>
2024-10-30 00:21:10 +0800

    temproarily modified the layout to match the requirements in server. this should be converted in to dto in future

--- app/app/_constants/strategy.ts
+++ app/app/_constants/strategy.ts
@@ -1,10 +1,10 @@
 import { Strategy } from "../_type/startegy";
 
 export const strategyList : Strategy[] = [
-    { name: 'Moving Average Convergence Divergence', symbol: 'MACD', params: ['short', 'long', 'signal'] },
+    { name: 'Moving Average Convergence Divergence', symbol: 'MACD', params: ['ema_s', 'ema_l', 'signal_mw'] },
     { name: 'Bollinger Band', symbol: 'BB', params: ['sma', 'dev'] },
-    { name: 'Simple Moving Average', symbol: 'SMA', params: ['sma S', 'sma M', 'sma L'] },
-    { name: 'Relative Strength Index', symbol: 'RSI', params: ['periods', 'rsi upper', 'rsi lower'] },
-    { name: 'RV', symbol: 'RV', params: ['volume low', 'volume high', 'return low', 'return high'] },
-    { name: 'SO', symbol: 'SO', params: ['period', 'd_mw'] },
+    { name: 'Simple Moving Average', symbol: 'SMA', params: ['sma_s', 'sma_m', 'sma_l'] },
+    { name: 'Relative Strength Index', symbol: 'RSI', params: ['periods', 'rsi_upper', 'rsi_lower'] },
+    { name: 'RV', symbol: 'RV', params: ['volume_thresh_low', 'volume_thresh_high', 'return_thresh_low', 'return_thresh_high'] },
+    { name: 'SO', symbol: 'SO', params: ['periods', 'd_mw'] },
 ];
